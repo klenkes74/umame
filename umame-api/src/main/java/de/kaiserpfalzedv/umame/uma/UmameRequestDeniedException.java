5ddb8263f34aec42b961fcf7e30eee7c3c807d1d
Roland T. Lichti <rlichti@redhat.com>
2020-02-09 12:58:37 +0100

    Added UmameRequestDeniedException and needed infrastructure.

---
+++ umame-api/src/main/java/de/kaiserpfalzedv/umame/uma/UmameRequestDeniedException.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.kaiserpfalzedv.umame.uma;
+
+import de.kaiserpfalzedv.umame.BaseUmameRuntimeException;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * If the resource server is unable to provide a permission ticket from the
+ * authorization server, then it includes a header of the following form in its
+ * response to the client: Warning: 199 - "UMA Authorization Server
+ * Unreachable".
+ *
+ * @author rlichti
+ * @version 1.0.0 2020-02-08
+ * @since 1.0.0 2020-02-08
+ */
+public class UmameRequestDeniedException extends BaseUmameRuntimeException {
+    private static final Response.Status STATUS = Response.Status.FORBIDDEN;
+
+    private AuthorizationServer as;
+
+    public UmameRequestDeniedException(AuthorizationServer as) {
+        super("{\n  \"error\": \"request_denied\"\n}");
+
+        this.as = as;
+    }
+
+    public Response.Status getStatus() {
+        return STATUS;
+    }
+
+    public AuthorizationServer getAS() { return as; }
+}
---
+++ umame-api/src/test/java/de/kaiserpfalzedv/umame/rs/UmameRequestDeniedExceptionMapperTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2020 Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.kaiserpfalzedv.umame.rs;
+
+import de.kaiserpfalzedv.umame.uma.UmameRequestDeniedException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static de.kaiserpfalzedv.umame.rs.TestObjects.AS;
+
+/**
+ * @author rlichti
+ * @version 1.0.0 2020-02-09
+ * @since 1.0.0 2020-02-09
+ */
+public class UmameRequestDeniedExceptionMapperTest {
+    private static final Logger LOG = LoggerFactory.getLogger(UmameRequestDeniedExceptionMapperTest.class);
+
+    private static final UmameRequestDeniedException EXCEPTION = new UmameRequestDeniedException(AS);
+
+    private UmameRequestDeniedExceptionMapper service;
+
+    @Test
+    public void shouldReturnAvalidResponseForPermissionNeededException() {
+        MDC.put("id", "valid-response");
+
+        Response result = service.toResponse(EXCEPTION);
+
+        LOG.debug("Status: {} ({})", result.getStatusInfo(), result.getStatus());
+        LOG.debug("Content type: {}", result.getMediaType());
+        LOG.debug("Cache control: {}", result.getHeaderString("Cache-control"));
+        LOG.debug("Content: {}", result.getEntity());
+
+
+        assert result.getStatus() == Response.Status.FORBIDDEN.getStatusCode();
+        assert result.getMediaType().equals(MediaType.APPLICATION_JSON_TYPE);
+        assert result.getHeaderString("Cache-control").equals("no-store");
+
+        assert result.getEntity().equals(EXCEPTION.getMessage());
+    }
+
+    @Test
+    public void shouldReturnTheStatusWhenExceptionIsTested() {
+        assert EXCEPTION.getStatus() == Response.Status.FORBIDDEN;
+        assert EXCEPTION.getAS().equals(AS);
+    }
+
+
+    @BeforeEach
+    public void setUpService() {
+        service = new UmameRequestDeniedExceptionMapper();
+    }
+}
